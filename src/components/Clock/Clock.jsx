fba0bd4fd8468f32d3360910f00cc85401b585f1
Tiziano Cappai <tiziano.cappai@accenture.com>
2023-10-19 14:05:59 +0200

    comment cloc with useMemo

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,45 +1,16 @@
 import { useEffect, useMemo, useState } from 'react';
 
-// const Clock = () => {
-// 	const [seconds, setSeconds] = useState(null);
-// 	const [minutes, setMinutes] = useState(null);
-// 	const [hours, setHours] = useState(null);
-
-// 	useEffect(() => {
-// 		setInterval(() => {
-// 			const date = new Date();
-// 			setSeconds(date.getSeconds());
-// 			setMinutes(date.getMinutes());
-// 			setHours(date.getHours());
-// 		}, 1000);
-// 	}, []);
-
-// 	return (
-// 		<div>
-// 			<span>
-// 				{hours} : {minutes} : {seconds}
-// 			</span>
-// 		</div>
-// 	);
-// };
-
 const Clock = () => {
-	const [date, setDate] = useState(new Date());
-	const seconds = useMemo(() => {
-		return date.getSeconds();
-	}, [date]);
-
-	const minutes = useMemo(() => {
-		return date.getMinutes();
-	}, [date]);
-
-	const hours = useMemo(() => {
-		return date.getHours();
-	}, [date]);
+	const [seconds, setSeconds] = useState(null);
+	const [minutes, setMinutes] = useState(null);
+	const [hours, setHours] = useState(null);
 
 	useEffect(() => {
 		setInterval(() => {
-			setDate(new Date());
+			const date = new Date();
+			setSeconds(date.getSeconds());
+			setMinutes(date.getMinutes());
+			setHours(date.getHours());
 		}, 1000);
 	}, []);
 
@@ -51,5 +22,35 @@ const Clock = () => {
 		</div>
 	);
 };
+
+// clock with useMemo
+// const Clock = () => {
+// 	const [date, setDate] = useState(new Date());
+// 	const seconds = useMemo(() => {
+// 		return date.getSeconds();
+// 	}, [date]);
+
+// 	const minutes = useMemo(() => {
+// 		return date.getMinutes();
+// 	}, [date]);
+
+// 	const hours = useMemo(() => {
+// 		return date.getHours();
+// 	}, [date]);
+
+// 	useEffect(() => {
+// 		setInterval(() => {
+// 			setDate(new Date());
+// 		}, 1000);
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			<span>
+// 				{hours} : {minutes} : {seconds}
+// 			</span>
+// 		</div>
+// 	);
+// };
 
 export default Clock;
